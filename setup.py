b6fb0fb7d6ac89b316dfb0fa787b7199fc35900f
Christian Dondrup <cdondrup@lincoln.ac.uk>
2014-11-19 20:39:56 +0000

    Clean-up

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=['marathon_gui'],
-    scripts=['scripts/robblog_node.py', 'scripts/set_alias.py', 'scripts/set_safe_points.py', 'scripts/web_interface.py'],
+    scripts=['scripts/web_interface.py', 'scripts/card_interface.py', 'scripts/execute_task.py', 'scripts/insert_behaviour.py'],
     package_dir={'': 'src'})
 
 setup(**setup_args)
